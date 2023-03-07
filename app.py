d94536b36d78aa7918ed51c8d80aab3561add9dc
IDGS-802-20001492 <quirozbernardo2@gmail.com>
2023-03-06 18:56:18 -0600

    Trabajo del día 06/03/2023
    Trabajo del día 06 de Marzo del 2023

--- app.py
+++ app.py
@@ -21,11 +21,43 @@ def index():
         db.session.commit()
     return render_template('index.html', form = create_form)
 
+@app.route("/modificar", methods = ['GET','POST'])
+def modificar():
+    mod_Form = forms.UserForm(request.form)
+    
+    if request.method == 'GET':
+        id = request.args.get('id')
+        alum1 = db.session.query(Alumnos).filter(Alumnos.id == id).first()
+        mod_Form.id.data = request.args.get('id')
+        mod_Form.nombre.data = alum1.nombre
+        mod_Form.apellidos.data = alum1.apellidos
+        mod_Form.email.data = alum1.email
+
+    if request.method == 'POST':
+        id = mod_Form.id.data
+        alum = db.session.query(Alumnos).filter(Alumnos.id == id).first()
+        alum.nombre = mod_Form.nombre.data
+        alum.apelidos = mod_Form.apellidos.data
+        alum.email = mod_Form.email.data
+        db.session.add(alum)
+        db.session.commit()
+        return redirect(url_for('ABCompleto'))
+        
+    return render_template('modificar.html',form = mod_Form)
+
+@app.route("/ABCompleto", methods = ['GET','POST'])
+def abc():
+    abc_Form = forms.UserForm(request.form)
+    alumno = Alumnos.query.all()
+
+    return render_template('ABCompleto.html', form = abc_Form, alumno = alumno)
+
+
 csrf = CSRFProtect()
 
 if __name__ == '__main__':
     csrf.init_app(app)
     db.init_app(app)
     with app.app_context():
-        db.create_all()
+        db.create_all #Se borró los ()
     app.run(port=3000)
